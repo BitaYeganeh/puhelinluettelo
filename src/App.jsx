a0b2461110b939913d37c50d822578a482bf9c79
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-15 20:24:25 +0300

    The Phonebook step 1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+
+
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+  event.preventDefault()  // stop the page from reloading
+
+  const personObject = { name: newName }
+
+  setPersons(persons.concat(personObject)) // update the list
+  setNewName('') // clear the input after adding
+}
+
+return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name:<input 
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+
+      </form>
+
+
+      <h2>Numbers</h2>
+        <ul>
+          {persons.map((person, index) => (
+            <li key={index}>{person.name}</li>
+          ))}
+        </ul>
+    </div>
+  )
+};
 
 export default App
